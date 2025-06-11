7a7827883796aa70d1fc80b2fab41e780eed4bb2
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-06-11 15:50:53 +0200

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get stzarted by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
